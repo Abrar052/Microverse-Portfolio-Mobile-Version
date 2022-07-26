801ef92307361867f9d9e69bf98fa1d68432f34d
Abrar052 <bc160400820@vu.edu.pk>
2022-07-27 02:08:14 +0500

    Added Cross button to the toggle Button

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 const navToggle = document.querySelector('#navToggle');
+const icon = document.querySelector('.fa-bars');
 const nav = document.querySelector('#nav-desktop');
 
 navToggle.addEventListener('click', () => {
   nav.classList.toggle('nav-open');
+  if (icon.classList.contains('fa-bars')) {
+    icon.classList.replace('fa-bars', 'fa-times');
+  } else {
+    icon.classList.replace('fa-times', 'fa-bars');
+  }
 });
 
 document.querySelectorAll('.nav-link').forEach(() => nav.addEventListener('click', () => {
